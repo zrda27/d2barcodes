6809b1262b51c336dec3f55fbab3174e20d5eb04
zengruda <zrda27@163.com>
2016-09-30 16:40:18 +0800

    导入到idea

--- src/test/java/test/net/netca/bpms/common/d2barcodes/TestD2Creator.java
+++ src/test/java/test/net/netca/bpms/common/d2barcodes/TestD2Creator.java
@@ -1,18 +1,17 @@
 package test.net.netca.bpms.common.d2barcodes;
 
+import net.netca.bpms.common.d2barcodes.D2Creator;
+import org.junit.Test;
+
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import org.junit.Test;
-
-import net.netca.bpms.common.d2barcodes.D2Creator;
-
 public class TestD2Creator {
 	@Test
 	public void testCreate() throws FileNotFoundException, IOException{
-		try(OutputStream os = new FileOutputStream("src/test/resources/d2.png");){
+		try(OutputStream os = new FileOutputStream(this.getClass().getResource("").getPath() + "/d2.png");){
 			D2Creator.create("hello world233", os);
 		}
 	}
--- src/test/java/test/net/netca/bpms/common/d2barcodes/TestD2Decoder.java
+++ src/test/java/test/net/netca/bpms/common/d2barcodes/TestD2Decoder.java
@@ -1,21 +1,19 @@
 package test.net.netca.bpms.common.d2barcodes;
 
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import net.netca.bpms.common.d2barcodes.D2Decoder;
+import org.junit.Test;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.junit.Test;
-
-import com.google.zxing.NotFoundException;
-import com.google.zxing.Result;
-
-import net.netca.bpms.common.d2barcodes.D2Decoder;
-
 public class TestD2Decoder {
 	@Test
 	public void testDecode() throws FileNotFoundException, IOException, NotFoundException{
-		try(InputStream is = new FileInputStream("src/test/resources/1475141701.png");){
+		try(InputStream is = new FileInputStream(this.getClass().getResource("").getPath() + "/d2.png");){
 			Result result = D2Decoder.decode(is);
 			System.out.println(result);
 		}
